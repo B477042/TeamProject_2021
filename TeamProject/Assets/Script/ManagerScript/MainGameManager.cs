4c7bebd5d0fecc92a9119649e016b662e1e20045
John <eett97@naver.com>
2021-05-13 14:56:17 +0900

    backup

---
+++ TeamProject/Assets/Script/ManagerScript/MainGameManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+*   Main Game Manager
+*   : singleton class
+*   
+
+*/
+
+
+public class MainGameManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // // Update is called once per frame
+    // void Update()
+    // {
+        
+    // }
+}
