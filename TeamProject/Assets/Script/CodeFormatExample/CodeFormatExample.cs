d6f7c347255addfd391480be88b9a72e5904460a
John <eett97@naver.com>
2021-05-15 20:25:50 +0900

    code fromet

---
+++ TeamProject/Assets/Script/CodeFormatExample/CodeFormatExample.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CodeFormatExample : MonoBehaviour
+{
+[SerializeField]
+public string House;
+[SerializeField]
+private string home;
+
+public static double Height;
+private static double Weight;
+
+    public string Name;
+    private int age;
+
+    public int n_Pepsi;
+    private int n_coke;
+
+
+public void GetID(int Input) 
+{
+
+ 
+}
+
+private void getPW(int InputParam)
+{
+
+}
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
