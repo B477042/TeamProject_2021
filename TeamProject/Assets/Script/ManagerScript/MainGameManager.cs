fb1b23bbe1cbcc670fea3025c4dad4854def0fe2
John <eett97@naver.com>
2021-05-17 00:00:29 +0900

    back up

--- TeamProject/Assets/Script/ManagerScript/MainGameManager.cs
+++ TeamProject/Assets/Script/ManagerScript/MainGameManager.cs
@@ -5,22 +5,53 @@ using UnityEngine;
 /*
 *   Main Game Manager
 *   : singleton class
-*   
-
+*   Main Features : 
+*   1) Record the score
+*   2) 
 */
 
 
 public class MainGameManager : MonoBehaviour
 {
+    
+    
+
+
+
+
+
+    private static  MainGameManager  instance = null;
+    public static  MainGameManager  Instance
+    {
+        get
+        {
+            return instance;
+        }
+    }
+
+    
+   
+    private void Awake()
+    {
+        if(instance!=null)
+        {
+            DestroyImmediate(gameObject);
+            return;
+        }
+        instance = this;
+       // Debug.LogWarning("Game manger instance Called");
+
+        DontDestroyOnLoad(this);
+    }
+
+
+
+
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    // // Update is called once per frame
-    // void Update()
-    // {
-        
-    // }
+   
 }
