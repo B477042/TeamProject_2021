5590e553d2eb85cc21c9f14ab1e6e200b783c5c5
zhengjun <65171413+Abe1o@users.noreply.github.com>
2021-05-30 20:58:57 +0900

    2021/5/30"play"&"bullet"

---
+++ TeamProject/Assets/Script/UIScript/TittleUIScript.cs
@@ -0,0 +1,110 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class TittleUIScript : MonoBehaviour
+{
+    [SerializeField]
+    public  AudioClip SFX_HoverSound;
+    [SerializeField]
+    public GameObject SFX_TittleBGM;
+    [SerializeField]
+    public GameObject UI_Options;
+
+    //Options Window Object
+    private static GameObject obj_Options;
+
+    private static GameObject obj_TittleBGM;
+    
+    //Public Button Click Sound Source Object
+    private static AudioSource buttonAudio;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        buttonAudio = gameObject.GetComponent<AudioSource>();
+        obj_TittleBGM = GameObject.Instantiate<GameObject>(SFX_TittleBGM);
+        obj_TittleBGM.GetComponent<AudioSource>().Play();
+        obj_TittleBGM.GetComponent<AudioSource>().loop = true;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnStartButton()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Game");
+    }
+
+    //Play Button Click Sound
+    public void HoverAtButton()
+    {
+        buttonAudio.Play(); 
+    }
+
+    //Close Options window
+    public void Click_CloseButton()
+    {
+        if (obj_Options)
+            obj_Options.SetActive(false);
+    }
+
+
+    public void Click_Options()
+    {
+
+        if (!obj_Options)
+            obj_Options = GameObject.Instantiate<GameObject>(UI_Options);
+        else
+            obj_Options.SetActive(true);
+    }
+
+    //Mute input param's sound
+    public void Click_Mute(GameObject muteObject)
+    {
+        //bool bIsMuted = muteObject.GetComponent<AudioSource>().mute;
+
+        //if (bIsMuted)
+        //    muteObject.GetComponent<AudioSource>().mute = false;
+        //else
+        //    muteObject.GetComponent<AudioSource>().mute = true;
+
+        if (muteObject == SFX_TittleBGM)
+            muteBGM();
+
+
+    }
+    public void Click_ExitGame()
+    {
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+
+        #else
+            Application.Quit(); 
+
+        #endif
+    }
+
+    public void Slide_BGM(Slider slider )
+    {
+        print(slider.value);
+        obj_TittleBGM.GetComponent<AudioSource>().volume = slider.value;
+
+    }
+
+    private void muteBGM()
+    {
+        bool bIsMuted = obj_TittleBGM.GetComponent<AudioSource>().mute;
+
+        if (bIsMuted)
+            obj_TittleBGM.GetComponent<AudioSource>().mute = false;
+        else
+            obj_TittleBGM.GetComponent<AudioSource>().mute = true;
+    }
+}
+ 
--- TeamProject/ReadMe.txt
+++ TeamProject/ReadMe.txt
@@ -3,3 +3,8 @@
 - Script : C# code만 넣어둘 것
 - Scenes : Scene 파일만 저장
 - Prefab : Prefab으로 만든 오브젝트 저장, 종류 별로 폴더를 만들어서 관리
+
+
+
+#Code 작성법
+- Script\example 
