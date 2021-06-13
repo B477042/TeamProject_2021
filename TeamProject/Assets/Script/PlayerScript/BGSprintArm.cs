dc5bea1c5c548a74a73f50141585a22627de6d87
B477042 <37619106+B477042@users.noreply.github.com>
2021-06-14 02:13:30 +0900

    back up

--- TeamProject/Assets/Script/PlayerScript/BGSprintArm.cs
+++ TeamProject/Assets/Script/PlayerScript/BGSprintArm.cs
@@ -2,30 +2,43 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+/*
+*   This script calculates which part of the background texture 
+*   you want to show based on the player's location.
+*   Select background texture from texture asset.
+*   Calculates the ratio between the position of the player and the two end points of the level
+*   This component is attached to the camera.
+*/
 public class BGSprintArm : MonoBehaviour
 {
-    public Vector3 pos_Start;
-    public Vector3 pos_End;
-    private Sprite BG;
+    //Leftmost point the player can go to
+    [SerializeField] private Vector3 Pos_Start;
+    //Righttmost point the player can go to
+    [SerializeField] private Vector3 Pos_End;
+    //Refer to the transform information for the object
+    [SerializeField] private GameObject Obj_Player;
+    [SerializeField] private Sprite BG;
+
     // Start is called before the first frame update
     void Start()
     {
-        setBG();
+        //setBG();
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        UpdateBG();
     }
 
-// Get BG Texture From GameObject's Sprite Renderer Component
+    // Get BG Texture From GameObject's Sprite Renderer Component
     void setBG()
     {
-        var comp_sprite = gameObject.GetComponent<SpriteRenderer>();
-        if(!comp_sprite){print("There isn't any sprite renderer in gameObject");return;}
-        BG = comp_sprite.sprite;
+        // var comp_sprite = gameObject.GetComponent<SpriteRenderer>();
+        // if(!comp_sprite){print("There isn't any sprite renderer in gameObject");return;}
+        // BG = comp_sprite.sprite;
         
     }
 
